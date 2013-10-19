d81fbfd488ec7fe165f8e8e65eedca259db9ac71
王超 <yueyoum@gmail.com>
2013-10-19 23:55:20 +0800

    调整 player.models

--- sanguo/apps/player/models.py
+++ sanguo/apps/player/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-# from django.utils import timezone
+from django.utils import timezone
 
 class User(models.Model):
     email = models.CharField(max_length=64, blank=True, db_index=True)
     passwd = models.CharField(max_length=40, blank=True)
     device_token = models.CharField(max_length=40, blank=True, db_index=True)
 
-    last_login = models.DateTimeField()
-    game_session = models.CharField(max_length=64)
+    last_login = models.DateTimeField(default=timezone.now())
 
     
     def is_bind(self):
