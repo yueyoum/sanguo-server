debca7d746076fc91e4db0e4c084f8f7099b4a96
王超 <yueyoum@gmail.com>
2013-10-23 16:07:34 +0800

    add character admin

---
+++ sanguo/apps/character/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from models import Character, CharHero
+from apps.hero.models import Hero
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'account_id', 'server_id', 'name',
+            'gold', 'gem', 'level', 'honor',
+            )
+
+    list_filter = ('account_id', 'server_id', )
+
+class CharHeroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'char_id', 'char_name', 'hero_id', 'hero_name',
+            'level', 'exp',
+            )
+
+    list_filter = ('char_id',)
+
+    def char_name(self, obj):
+        return Character.objects.get(id=obj.char_id).name
+
+    def hero_name(self, obj):
+        return Hero.objects.get(id=obj.hero_id).name
+
+
+
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(CharHero, CharHeroAdmin)
+
