a95b47beeef1275a757d66c6fc9bd6e946048d69
王超 <yueyoum@gmail.com>
2013-10-23 18:37:01 +0800

    adjust

--- sanguo/start_dev_server.sh
+++ sanguo/start_dev_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:../
-
 python manage.py runserver 0.0.0.0:8000
 
--- sanguo/start_test_server.sh
+++ sanguo/start_test_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:../
-
-python manage.py runserver 0.0.0.0:8000 --settings=sanguo.test_settings
+python manage.py runserver --settings=sanguo.test_settings
 
