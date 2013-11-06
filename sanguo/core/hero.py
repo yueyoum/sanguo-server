7b16940abe6fc67072dfd7b90406af315660431c
王超 <yueyoum@gmail.com>
2013-11-06 16:44:25 +0800

    adjust

--- sanguo/core/hero.py
+++ sanguo/core/hero.py
@@ -1,4 +1,3 @@
-from apps.character.models import CharHero
 from core import GLOBAL
 
 
@@ -12,16 +11,20 @@ def cal_hero_property(original_id, level):
 
 
 class Hero(object):
-    def __init__(self, hid, original_id, exp):
+    def __init__(self, hid, original_id, exp, skills):
         self.id = hid
         self.original_id = original_id 
         self.exp = exp
+        self.skills = skills
 
         self.level, self.current_exp, self.next_level_exp = \
                 GLOBAL.LEVEL_TOTALEXP[self.exp]
 
         self.attack, self.defense, self.hp = \
                 cal_hero_property(self.original_id, self.level)
+
+        self.crit = 20
+        self.dodge = 20
 
         self.additional_attributes()
 
