fd95f03d20f1381a48f7899c123727369c3ec58a
王超 <yueyoum@gmail.com>
2014-01-10 15:54:48 +0800

    adjust

--- sanguo/sanguo/settings.py
+++ sanguo/sanguo/settings.py
@@ -11,8 +11,9 @@ except (KeyError, ValueError):
         TESTING = False
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-TMP_PATH = os.path.normpath(os.path.join(CURRENT_PATH, '../../tmp'))
-LOG_PATH = os.path.normpath(os.path.join(CURRENT_PATH, '../logs'))
+PROJECT_PATH = os.path.dirname(CURRENT_PATH)
+TMP_PATH = os.path.normpath(os.path.join(PROJECT_PATH, '../tmp'))
+LOG_PATH = os.path.normpath(os.path.join(PROJECT_PATH, 'logs'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -76,7 +77,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
