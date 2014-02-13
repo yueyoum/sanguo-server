4710697c2dd49d7bb557fe9c445c176967f6c039
王超 <yueyoum@gmail.com>
2014-02-13 11:21:30 +0800

    adjust

--- sanguo/apps/achievement/admin.py
+++ sanguo/apps/achievement/admin.py
@@ -11,7 +11,7 @@ class AchievementResources(resources.ModelResource):
 
 
 class AchievementAdmin(ImportExportModelAdmin):
-    list_display = ('id', 'tp', 'tp_name', 'des', 'mode',
+    list_display = ('id', 'tp', 'tp_name', 'name', 'des', 'mode',
     'condition_id', 'condition_name', 'condition_value',
     'sycee', 'buff_id', 'buff_name', 'buff_value'
     )
