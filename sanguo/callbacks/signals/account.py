05207a2b7998ec370cf3ac7390cba00bdf56eafe
王超 <yueyoum@gmail.com>
2014-02-17 11:06:37 +0800

    去掉设置server status的异步调用

--- sanguo/callbacks/signals/account.py
+++ sanguo/callbacks/signals/account.py
@@ -5,7 +5,7 @@ from core.notify import login_notify
 from worker import tasks
 
 def login(account_id, server_id, char_id, **kwargs):
-    tasks.update_server_status.apply_async(args=[server_id], kwargs={'login_times': 1})
+    # tasks.update_server_status.apply_async(args=[server_id], kwargs={'login_times': 1})
     if char_id:
         login_notify(char_id)
 
