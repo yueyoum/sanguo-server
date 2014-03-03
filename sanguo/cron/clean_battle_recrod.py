216e04e74b756e799bc448e2547b432103e20ed9
王超 <yueyoum@gmail.com>
2014-03-03 11:09:54 +0800

    add clean_battle_recrod cronjob

---
+++ sanguo/cron/clean_battle_recrod.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+
+from _base import Logger
+
+from django.conf import settings
+from utils.timezone import localnow
+
+DAYS_DIFF = 7
+
+def clean():
+    now = localnow()
+    DAY = now - datetime.timedelta(days=DAYS_DIFF)
+
+    BATTLE_RECORD_PATH = settings.BATTLE_RECORD_PATH
+    os.chdir(BATTLE_RECORD_PATH)
+    amount = 0
+    files = os.listdir('.')
+    for f in files:
+        t = os.path.getctime(f)
+        create_date = datetime.datetime.fromtimestamp(t)
+        if create_date < DAY:
+            os.unlink(f)
+            amount += 1
+
+    logger = Logger('clean_battle_record.log')
+    logger.write("Clean Battle Record Done. Amount: {0}".format(amount))
+    logger.close()
+
+
+if __name__ == '__main__':
+    clean()
+
+
+
---
+++ sanguo/cron/clean_battle_recrod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SELF=$(readlink -f $0)
+PROJECT_PATH=$(dirname $(dirname $(dirname $SELF)))
+cd $PROJECT_PATH
+
+source activate_env
+cd sanguo
+
+python cron/clean_battle_record.py
+
+exit 0
+
