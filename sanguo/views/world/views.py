be05efd7bac53ea669fac20d05a058383d258cf4
王超 <yueyoum@gmail.com>
2014-03-10 18:00:46 +0800

    add operate_guard to sync

--- sanguo/views/world/views.py
+++ sanguo/views/world/views.py
@@ -3,13 +3,14 @@
 __author__ = 'Wang Chao'
 __date__ = '2/21/14'
 
-from utils.decorate import message_response
+from utils.decorate import message_response, operate_guard
 
 from utils import timezone
 from utils import pack_msg
 from protomsg import SyncResponse
 
 @message_response("SyncResponse")
+@operate_guard('sync', 3, keep_result=False)
 def sync(request):
     msg = SyncResponse()
     msg.ret = 0
