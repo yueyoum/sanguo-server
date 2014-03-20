9f219c6a224f21dcc1889667aa08dc298457f852
王超 <yueyoum@gmail.com>
2014-03-20 15:56:35 +0800

    fix Achievement Models

--- sanguo/apps/achievement/models.py
+++ sanguo/apps/achievement/models.py
@@ -34,9 +34,9 @@ class Achievement(models.Model):
     buff_name = models.CharField("BUFF名称", max_length=32, blank=True)
     buff_value = models.IntegerField("BUFF值", null=True, blank=True)
 
-    equipments = models.CharField("装备", blank=True)
-    gems = models.CharField("宝石", blank=True)
-    stuffs = models.CharField("材料", blank=True)
+    equipments = models.CharField("装备", max_length=255, blank=True)
+    gems = models.CharField("宝石", max_length=255, blank=True)
+    stuffs = models.CharField("材料", max_length=255, blank=True)
 
     def __unicode__(self):
         return u'<成就: %s>' % self.name
