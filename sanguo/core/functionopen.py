ff9a21b2fc120c4bbb6d7e9bcc7680fbe11d6bed
王超 <yueyoum@gmail.com>
2014-04-15 14:08:42 +0800

    adjust FunctionOpen

--- sanguo/core/functionopen.py
+++ sanguo/core/functionopen.py
@@ -39,11 +39,17 @@ class FunctionOpen(object):
             self.mf.save()
 
 
-    def trig(self, char_level, stage_id):
+    def trig(self, char_level):
+        try:
+            s = MongoStage.objects.get(id=self.char_id)
+            passed_stages = s.stages
+        except DoesNotExist:
+            passed_stages = []
+
         opened_funcs = []
         for func_id in self.mf.freeze[:]:
             this_func = FUNCTION_OPEN_FUNCS_REV[func_id]
-            if char_level >= this_func.char_level and stage_id >= this_func.stage_id:
+            if char_level >= this_func.char_level and str(this_func.stage_id) in passed_stages:
                 # OPEN
                 self.mf.freeze.remove(func_id)
                 opened_funcs.append(func_id)
@@ -53,7 +59,7 @@ class FunctionOpen(object):
 
         f = Formation(self.char_id)
         for v in FUNCTION_OPEN_SOCKETS.values():
-            if char_level >= v.char_level and stage_id >= v.stage_id:
+            if char_level >= v.char_level and str(v.stage_id) in passed_stages:
                 f.open_socket(v.socket_amount)
                 if 20 not in opened_funcs:
                     opened_funcs.append(20)
@@ -62,16 +68,13 @@ class FunctionOpen(object):
 
 
     def trig_by_char_level(self, char_level):
-        s = MongoStage.objects.get(id=self.char_id)
-        stage_ids = [int(i) for i in s.stages.keys()]
-        stage_id = max(stage_ids)
-        return self.trig(char_level, stage_id)
+        return self.trig(char_level)
 
 
-    def trig_by_stage_id(self, stage_id):
+    def trig_by_stage(self):
         c = MongoCharacter.objects.get(id=self.char_id)
         char_level = c.level
-        return self.trig(char_level, stage_id)
+        return self.trig(char_level)
 
 
     def send_notify(self):
