b91c6e0fa8b8c9ae7b3a7d5d2b67e57dec178157
王超 <yueyoum@gmail.com>
2014-05-07 13:39:26 +0800

    rm dll folder

--- dll/equipment.h
+++
@@ -1,8 +0,0 @@
-
-// 参数： 基础值，强化等级，属性增长
-
-int equip_attack(int base_value, int level, int growing);
-int equip_defense(int base_value, int level, int growing);
-int equip_hp(int base_value, int level, int growing);
-
-
