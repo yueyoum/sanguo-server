7214b2c050902190ed42a8db535206dd1839ce4c
王超 <yueyoum@gmail.com>
2014-05-29 12:38:31 +0800

    cron

--- sanguo/cron/reset.sh
+++ sanguo/cron/reset.sh
@@ -6,7 +6,11 @@ cd $PROJECT_PATH
 source activate_env
 cd sanguo
 
+python cron/reset_counter.py
+python cron/reset_checkin.py
+python cron/reset_stage_elite.py
 python cron/reset_task.py
+python cron/reset_store_player_limit.py
 
 exit 0
 
