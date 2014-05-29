5e419adcabc83faf6d692285efb4086cf4ae4632
王超 <yueyoum@gmail.com>
2014-05-29 18:14:15 +0800

    CMD

--- sanguo/views/cmd.py
+++ sanguo/views/cmd.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from core.attachment import make_standard_drop_from_template
 from core.resource import Resource
+from core.functionopen import FunctionOpen
 
 def cmd(request):
     req = request._proto
@@ -30,10 +31,17 @@ def cmd(request):
         drop['stuffs'].append((req.param, 1))
     elif req.tp == 9:
         drop['souls'].append((req.param, 1))
+    elif req.tp == 10:
+        drop['purchase_got'] = req.param
+        drop['purchase_actual_got'] = req.param
 
 
-    resource = Resource(char_id, "CMD")
+    resource = Resource(char_id, "CMD", "tp: {0}, param: {1}".format(req.tp, req.param))
     standard_drop = resource.add(**drop)
     print standard_drop
+
+    if req.tp == 11:
+        fo = FunctionOpen(char_id)
+        fo._open(req.param)
 
     return HttpResponse('', content_type='text/plain')
