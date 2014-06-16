eed3ebf87467a697aef992fc7a558eaa46bf6bf6
王超 <yueyoum@gmail.com>
2014-06-16 16:33:29 +0800

    Fix Login Notify

--- sanguo/core/msgpipe.py
+++ sanguo/core/msgpipe.py
@@ -5,13 +5,12 @@ def publish_to_char(char_id, msg):
     redis_client_two.rpush('noti:{0}'.format(char_id), msg)
 
 
-def message_get(char_id, is_login=False):
+def message_get(char_id):
     key = 'noti:{0}'.format(char_id)
-
-    if is_login:
-        msgs = []
-    else:
-        msgs = redis_client_two.lrange(key, 0, -1)
-
+    msgs = redis_client_two.lrange(key, 0, -1)
     redis_client_two.delete(key)
     return msgs
+
+def message_clean(char_id):
+    key = 'noti:{0}'.format(char_id)
+    redis_client_two.delete(key)
--- sanguo/sanguo/middleware.py
+++ sanguo/sanguo/middleware.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 
 import protomsg
 
-from core.msgpipe import message_get
-# from core.timercheck import timercheck
+from core.msgpipe import message_get, message_clean
 from core.activeplayers import ActivePlayers
 
 ### FOR DEBUG
@@ -30,6 +29,10 @@ class UnpackAndVerifyData(RequestFilter):
             ap = ActivePlayers(request._server_id)
             ap.set(request._char_id)
 
+        if char_id and request.path == '/player/login/':
+            message_clean(char_id)
+
+
 class PackMessageData(object):
     def process_response(self, request, response):
         if response.status_code != 200:
@@ -38,18 +41,9 @@ class PackMessageData(object):
         if request.path.startswith('/api/'):
             return response
 
-        is_login = False
-        if request.path.startswith('/login/'):
-            is_login = True
-
         char_id = getattr(request, '_char_id', None)
         if char_id:
-            #if char_id not in _BIND:
-            #    rabbit.bind(char_id, request._server_id)
-            #    _BIND.add(char_id)
-
-            #other_msgs = rabbit.message_get_all(char_id)
-            other_msgs = message_get(char_id, is_login=is_login)
+            other_msgs = message_get(char_id)
         else:
             other_msgs = []
 
@@ -75,19 +69,3 @@ class PackMessageData(object):
         print _msg_type
         # DEBUG END
         return HttpResponse(data, content_type='text/plain')
-
-#
-# class ViewExceptionHandler(object):
-#     def process_exception(self, request, exception):
-#         if isinstance(exception, SanguoException):
-#             proto = getattr(protomsg, exception.response_msg_name)
-#             m = proto()
-#             m.ret = exception.error_id
-#
-#             data = pack_msg(m)
-#             return HttpResponse(data, content_type='text/plain')
-#
-#         traceback.print_exc()
-#         raise exception
-#
-
