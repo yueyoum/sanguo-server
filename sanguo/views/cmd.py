05f9021b0351663b65a83370d331a8b25d162e21
王超 <yueyoum@gmail.com>
2014-07-02 16:12:41 +0800

    add cmd check

--- sanguo/views/cmd.py
+++ sanguo/views/cmd.py
@@ -4,13 +4,15 @@ from core.attachment import make_standard_drop_from_template
 from core.resource import Resource
 from core.functionopen import FunctionOpen
 
+from preset.data import HEROS, EQUIPMENTS, GEMS, STUFFS
+
 def cmd(request):
     req = request._proto
     char_id = request._char_id
 
     if req.action == 2:
         print "Unsupported"
-        return
+        return HttpResponse('', content_type='text/plain')
 
     drop = make_standard_drop_from_template()
     if req.tp == 1:
@@ -21,16 +23,32 @@ def cmd(request):
         drop['gold'] = req.param
     elif req.tp == 4:
         drop['sycee'] = req.param
+
     elif req.tp == 5:
+        if req.param not in EQUIPMENTS:
+            return HttpResponse('', content_type='text/plain')
         drop['equipments'].append((req.param, 1, 1))
+
     elif req.tp == 6:
+        if req.param not in GEMS:
+            return HttpResponse('', content_type='text/plain')
         drop['gems'].append((req.param, 1))
+
     elif req.tp == 7:
+        if req.param not in HEROS:
+            return HttpResponse('', content_type='text/plain')
         drop['heros'].append((req.param, 1))
+
     elif req.tp == 8:
+        if req.param not in STUFFS:
+            return HttpResponse('', content_type='text/plain')
         drop['stuffs'].append((req.param, 1))
+
     elif req.tp == 9:
+        if req.param not in HEROS:
+            return HttpResponse('', content_type='text/plain')
         drop['souls'].append((req.param, 1))
+
     elif req.tp == 10:
         drop['purchase_got'] = req.param
         drop['purchase_actual_got'] = req.param
