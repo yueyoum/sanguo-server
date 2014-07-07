a78e26dccf8d3bfbbcc6733f4da144d3a62c0a45
王超 <yueyoum@gmail.com>
2014-07-07 15:07:47 +0800

    NOT emit logs to logman

--- sanguo/utils/log.py
+++ sanguo/utils/log.py
@@ -7,21 +7,21 @@ import json
 import struct
 import logging
 
-import zmq
-import msgpack
+# import zmq
+# import msgpack
 
 import arrow
 from django.conf import settings
 
 
-fmt = struct.Struct('>i')
-LOG_SYSTEM_ID = fmt.pack(1)
-LOG_RESOURCE_ID = fmt.pack(2)
-
-context = zmq.Context()
-sock = context.socket(zmq.PUSH)
-
-sock.connect("tcp://{0}:{1}".format(settings.LOG_MAN_HOST, settings.LOG_MAN_PORT))
+# fmt = struct.Struct('>i')
+# LOG_SYSTEM_ID = fmt.pack(1)
+# LOG_RESOURCE_ID = fmt.pack(2)
+#
+# context = zmq.Context()
+# sock = context.socket(zmq.PUSH)
+#
+# sock.connect("tcp://{0}:{1}".format(settings.LOG_MAN_HOST, settings.LOG_MAN_PORT))
 
 logger = logging.getLogger('sanguo')
 
@@ -29,50 +29,51 @@ TIME_ZONE = settings.TIME_ZONE
 
 class LogManHandler(logging.Handler):
     def emit(self, record):
-        if record.log_type_id == 1:
-            data = _make_system_data(record)
-        elif record.log_type_id == 2:
-            data = _make_resource_data(record)
-        else:
-            print "NOT SUPPORTED LOG TYPE ID", record.log_type_id
-            return
+        pass
+        # if record.log_type_id == 1:
+        #     data = _make_system_data(record)
+        # elif record.log_type_id == 2:
+        #     data = _make_resource_data(record)
+        # else:
+        #     print "NOT SUPPORTED LOG TYPE ID", record.log_type_id
+        #     return
 
         # sock.send(data)
 
 
-def _make_system_data(record):
-    data = {
-        'levelname': record.levelname,
-        'error_id': record.error_id,
-        'char_id': record.char_id,
-        'func_name': record.func_name,
-        'msg': record.error_msg,
-        'occurred_at': record.occurred_at,
-    }
-
-    binary = msgpack.packb(data)
-    return '%s%s' % (LOG_SYSTEM_ID, binary)
-
-def _make_resource_data(record):
-    data = {
-        'char_id': record.char_id,
-        'income': record.income,
-        'func_name': record.func_name,
-        'exp': record.exp,
-        'official_exp': record.official_exp,
-        'gold': record.gold,
-        'sycee': record.sycee,
-        'heros': json.dumps(record.heros),
-        'souls': json.dumps(record.souls),
-        'equipments': json.dumps(record.equipments),
-        'gems': json.dumps(record.gems),
-        'stuffs': json.dumps(record.stuffs),
-        'des': record.des,
-        'occurred_at': record.occurred_at,
-    }
-
-    binary = msgpack.packb(data)
-    return '%s%s' % (LOG_RESOURCE_ID, binary)
+# def _make_system_data(record):
+#     data = {
+#         'levelname': record.levelname,
+#         'error_id': record.error_id,
+#         'char_id': record.char_id,
+#         'func_name': record.func_name,
+#         'msg': record.error_msg,
+#         'occurred_at': record.occurred_at,
+#     }
+#
+#     binary = msgpack.packb(data)
+#     return '%s%s' % (LOG_SYSTEM_ID, binary)
+#
+# def _make_resource_data(record):
+#     data = {
+#         'char_id': record.char_id,
+#         'income': record.income,
+#         'func_name': record.func_name,
+#         'exp': record.exp,
+#         'official_exp': record.official_exp,
+#         'gold': record.gold,
+#         'sycee': record.sycee,
+#         'heros': json.dumps(record.heros),
+#         'souls': json.dumps(record.souls),
+#         'equipments': json.dumps(record.equipments),
+#         'gems': json.dumps(record.gems),
+#         'stuffs': json.dumps(record.stuffs),
+#         'des': record.des,
+#         'occurred_at': record.occurred_at,
+#     }
+#
+#     binary = msgpack.packb(data)
+#     return '%s%s' % (LOG_RESOURCE_ID, binary)
 
 
 def system_logger(error_id, char_id, func_name, error_msg):
