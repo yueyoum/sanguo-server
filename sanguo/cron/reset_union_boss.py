6a58421472181066e46b822faa7afab0f528fae2
王超 <yueyoum@gmail.com>
2014-12-15 19:58:02 +0800

    add reset_union_boss.py

---
+++ sanguo/cron/reset_union_boss.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wang Chao'
+__date__ = '14-12-15'
+
+import uwsgidecorators
+from cron.log import Logger
+
+from core.mongoscheme import MongoUnionBoss
+
+@uwsgidecorators.cron(0, 0, -1, -1, -1)
+def reset(signum):
+    logger = Logger("reset_union_boss.log")
+    logger.write("Start.")
+
+    for mb in MongoUnionBoss.objects.all():
+        mb.opended = {}
+        mb.save()
+
+    logger.write("Complete.")
