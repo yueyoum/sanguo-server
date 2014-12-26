874c212c9e26ef46a94aef56f1ca0775a0ab763c
王超 <yueyoum@gmail.com>
2014-12-26 13:23:16 +0800

    make version in libs

---
+++ sanguo/libs/version.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wang Chao'
+__date__ = '14-12-26'
+
+class Version(object):
+    __slots__ = ['version',]
+    def __init__(self, version):
+        self.version = version
+
+    def set_version(self, version):
+        print "==== VERSION_CHANGE ===="
+        print "==== old: {0} ====".format(self.version)
+        self.version = version
+        print "==== new: {0} ====".format(self.version)
+
+    def _make_version_to_tuple(self, version):
+        return tuple([int(i) for i in version.split(',')])
+
+    def is_valid(self, version):
+        self_version = self._make_version_to_tuple(self.version)
+        target_version = self._make_version_to_tuple(version)
+        return target_version >= self_version
+
+    def is_little_than(self, version):
+        self_version = self._make_version_to_tuple(self.version)
+        target_version = self._make_version_to_tuple(version)
+
+        self_major = self_version[:3]
+        target_major = target_version[:3]
+        return self_major < target_major
+
+version = Version("0.0.0.0")
+
