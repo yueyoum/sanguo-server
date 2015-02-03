a2e44e196bc55778aad367fa4dec523d29b0ac7c
王超 <yueyoum@gmail.com>
2015-02-03 15:07:44 +0800

    redis noti:{} key set expire to reduce memory use

--- sanguo/core/msgpipe.py
+++ sanguo/core/msgpipe.py
@@ -1,17 +1,22 @@
 from core.drives import redis_client
 
+EXPIRE = 3600
+
+keygen = lambda char_id: 'noti:{0}'.format(char_id)
 
 def publish_to_char(char_id, msg):
-    redis_client.rpush('noti:{0}'.format(char_id), msg)
+    key = keygen(char_id)
+    redis_client.rpush(key, msg)
+    redis_client.expire(key, EXPIRE)
 
 
 def message_get(char_id):
-    key = 'noti:{0}'.format(char_id)
+    key = keygen(char_id)
     msgs = redis_client.lrange(key, 0, -1)
     redis_client.delete(key)
-
     return msgs
 
+
 def message_clean(char_id):
-    key = 'noti:{0}'.format(char_id)
+    key = keygen(char_id)
     redis_client.delete(key)
