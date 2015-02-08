d22bb1dd4906054992b640b2b3ffd17a5d8620e4
王超 <yueyoum@gmail.com>
2015-02-09 00:13:41 +0800

    Fix, vip change and change the plunder current times

--- sanguo/callbacks/signals/vip.py
+++ sanguo/callbacks/signals/vip.py
@@ -7,7 +7,9 @@ from core.signals import vip_changed_signal, new_purchase_signal
 from core.prison import Prison
 from core.vip import VIP
 from core.mail import Mail
+from core.plunder import Plunder
 from preset.settings import MAIL_VIP_CHANGED_CONTENT, MAIL_VIP_CHANGED_TITLE
+from preset.data import VIP_FUNCTION
 
 def _new_purchase(char_id, new_got, total_got, **kwargs):
     VIP(char_id).send_notify()
@@ -16,6 +18,12 @@ def _vip_change(char_id, old_vip, new_vip, **kwargs):
     Prison(char_id).vip_changed(new_vip)
     m = Mail(char_id)
     m.add(name=MAIL_VIP_CHANGED_TITLE, content=MAIL_VIP_CHANGED_CONTENT.format(new_vip))
+
+    # 增加掠夺次数
+    plunder_times_change_value = VIP_FUNCTION[new_vip].plunder - VIP_FUNCTION[old_vip].plunder
+    if plunder_times_change_value:
+        plunder = Plunder(char_id)
+        plunder.change_current_plunder_times(plunder_times_change_value, allow_overflow=True)
 
 
 vip_changed_signal.connect(
